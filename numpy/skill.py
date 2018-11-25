921cd211d87494be75f9a3a5acd831bf091a599e
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-11-25 13:17:02 +0800

    Update skill.py

--- numpy/skill.py
+++ numpy/skill.py
@@ -4,5 +4,10 @@
  x = np.stack((x1, x2), axis=2)
  #x的shape是[51,51,2],生成了新的轴，即第三个维度
  #但是np.concatenate只能在已有的维度上做连接
- 
  print(x.shape)
+ import numpy as np
+data=np.identity(5)#[5,5]
+data=np.expand_dims(data,axis=2)#[5,5,1]
+print(data.shape)
+data=np.squeeze(data,axis=2)#[5,5]
+print(data.shape)
