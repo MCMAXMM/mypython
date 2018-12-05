a5763c644f82f8cc75c6b9139e169cfa088c882c
MCMAXMM <32216793+MCMAXMM@users.noreply.github.com>
2018-12-05 18:00:20 +0800

    Create my_pickle.py

---
+++ py_library/my_pickle.py
@@ -0,0 +1,9 @@
+import pickle
+#pickle可以将各种数据结构保存存二进制数据
+#节省读取时间
+# a={1:2,2:5,4:6,3:7,34:23}
+# with open("./ma.pkl","wb") as f:
+#     pickle.dump(a,f,protocol=pickle.HIGHEST_PROTOCOL)
+# with open("./ma.pkl","rb") as f:
+#    a=pickle.load(f)
+# print(a)
